af1e7a69436e20d2d283976a8d1b920bb84249b3
Baskin Tapkan <baskint@gmail.com>
2014-06-01 10:48:25 -0500

    added iOS project

---
+++ BoardSports-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BoardSports-iOS
+//
+//  Created by Baskin Tapkan on 6/1/14.
+//  Copyright (c) 2014 Baskin Tapkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BoardSports-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BoardSports-iOS
+//
+//  Created by Baskin Tapkan on 6/1/14.
+//  Copyright (c) 2014 Baskin Tapkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
